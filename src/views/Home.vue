8b8b80a4d1634a187abedfe11267c48b2fcd451e
ashwinshivam <111960991+ashwinshivam@users.noreply.github.com>
2023-04-21 21:21:17 +0530

    change

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<style scoped>
+
+</style>
+
+<template>
+
+<div>
+  <h1>Welcome to movies!</h1>
+  <input type="text" placeholder="Search by movie name"/>
+  <div style="width: 100px" v-for="movie in movies" :key="movie.id">
+    {{movie.title}}
+  </div>
+</div>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: 'home',
+    mounted() {
+        axios({
+          method: "GET",
+          "url": "assets/json/movies.json"
+        }).then(response => {
+          this.movies = JSON.parse(JSON.stringify(response.data));
+        }, error => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    data() {
+        return {
+          movies:[]
+        }
+    },
+    components: {},
+    methods: {}
+}
+
+</script>
